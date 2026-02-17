db768ea282c58b42a5297be5b1eead4ba90a6358
XDumka1 <kos.churin@mail.ru>
2026-02-18 00:39:31 +0400

    Добавление функции увелечения разрешения.py
    Добавление функции увелечения разрешения

--- bot.py
+++ bot.py
@@ -3,15 +3,25 @@ import os
 from config import TOKEN
 from rembg import remove
 from PIL import Image
+import subprocess
 
 bot = telebot.TeleBot(TOKEN)
-
+project_folder = os.path.dirname(os.path.abspath(__file__))
+project_folder = rf'{project_folder}\REAL-ESRGAN'
 user_choices = {}
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_id = message.from_user.id
-    bot.send_message(message.chat.id, "Привет, я бот для работы с фото")
+    bot.send_message(message.chat.id, 
+    """
+Привет! Я бот для обработки фото!
+1. Выбери нужное действие на кнопках ниже
+2. Отправь фото одним сообщением
+Доступные функции:
+• Удалить фон
+• Улучшить разрешение
+    """)
     keyboard = telebot.types.InlineKeyboardMarkup()
     button_Remove = telebot.types.InlineKeyboardButton(text="Удалить фон", callback_data='RemoveBackground')
     button_Upscale = telebot.types.InlineKeyboardButton(text="Улучшить разрешение",callback_data='Upscale')
@@ -21,9 +31,10 @@ def handle_start(message):
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     user_id = message.from_user.id
-    user_folder = str(user_id)
-    photo_name = 'photo.jpg'
+    user_folder = f'REAL-ESRGAN/USERS/{str(user_id)}'
     choice = user_choices.get(user_id)
+    input_path = os.path.join(user_folder, 'photo.jpg')
+    output_path = os.path.join(user_folder, 'output.png')
     
     if not os.path.exists(user_folder):
         os.makedirs(user_folder)
@@ -38,25 +49,38 @@ def handle_photo(message):
     if choice == "RemoveBackground":
         bot.send_message(message.chat.id, "Удаляем фон!")
 
-        input_path = os.path.join(user_folder, photo_name)
-        output_path = os.path.join(user_folder, 'output.png')
-
         input_img = Image.open(input_path)
         output_img = remove(input_img)
         output_img.save(output_path)
 
+        
         with open(output_path, 'rb') as file:
             bot.send_document(message.chat.id, file)
-
         os.remove(input_path)
         os.remove(output_path)
         del user_choices[user_id]
 
     elif choice == "Upscale":
         bot.send_message(message.chat.id, "Увеличиваем разрешение!")
+        original_dir = os.getcwd()
+        os.chdir(project_folder)
+        command = [
+            'realesrgan-ncnn-vulkan.exe', 
+            '-i', rf'USERS\{user_id}\photo.jpg', 
+            '-o', rf'USERS\{user_id}\output.png'
+        ]
+        subprocess.run(command)
+        os.chdir(original_dir)
+
+        with open(output_path, 'rb') as file:
+            bot.send_document(message.chat.id, file)
+        os.remove(input_path)
+        os.remove(output_path)
         del user_choices[user_id]
+
     else:
         bot.send_message(message.chat.id, "Выберите действие!")
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
